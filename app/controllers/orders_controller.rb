67f929f1e67b20e3532c2ef23435e21d454ac22f
Eric An <erican@me.com>
2015-10-07 21:58:43 -0700

    create a form_for a new order and form_tag search on navbar, update CSS styling for new order and search option

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,15 +1,16 @@
 class OrdersController < ApplicationController
 
-def index
-end
+	def index
+	end
 
-def show
-end
+	def show
+	end
 
-def new
-end
+	def new
+	  @order = Order.new
+	end
 
-def create
-end
+	def create
+	end
 
 end
