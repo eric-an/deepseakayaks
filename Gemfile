b0d569d972344c058264bb7c711c4a56add3e397
Eric An <erican@me.com>
2015-10-08 19:32:09 -0700

    exercise 40: add page authorizations

--- Gemfile
+++ Gemfile
@@ -27,6 +27,10 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem 'devise'
+
+gem 'devise_security_extension'
+
+gem 'cancancan'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,4 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 	has_many :orders
 
-  validate :password_complexity
-
-  def password_complexity
-  	if password.present? and not password.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d). /)
-  		errors.add :password, "must include at least one lowercase letter, one uppercase letter, and one digit"
-  	end
-  end
 end
