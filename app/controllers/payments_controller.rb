a4403793154ba28dd67beb8037e79ed63c6acc0f
Eric An <erican@me.com>
2015-10-21 14:45:26 -0700

    minor fix to Stripe implementation to dynamically include user email and name

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -3,20 +3,27 @@ class PaymentsController < ApplicationController
   def create
   	token = params[:stripeToken]
     @product = Product.find(params[:product_id])
+    @user = current_user
 
-		charge = Stripe::Charge.create(
-			:amount => @product.price * 100,
-			:currency => "usd",
-			:description => @product.description,
-      :source => token
-			)
+    customer = Stripe::Customer.create(
+      :source => token,
+      description: @user.first_name + " " + @user.last_name,
+      email: @user.email
+      )
+
+  		charge = Stripe::Charge.create(
+  			:amount => @product.price * 100,
+  			:currency => "usd",
+  			:description => @product.description,
+        :customer => customer.id
+  			)
 
     if charge.paid
       Order.create(
-        :total => @product.price * 100,
+        :total => @product.price,
         :product_id => @product.id,
-        :user_id => @user_id
-)
+        :user_id => @user.id
+      )
     end
 
   	rescue Stripe::CardError => e
@@ -27,3 +34,16 @@ class PaymentsController < ApplicationController
       redirect_to product_path(@product)
   end
 end
+
+
+# # save the customer ID in your database so you can use it later
+# save_stripe_customer_id(user, customer.id)
+
+# # later
+# customer_id = get_stripe_customer_id(user)
+
+# Stripe::Charge.create(
+#     amount: 1500, # $15.00 this time
+#     currency: 'usd',
+#     customer: customer_id
+# )
