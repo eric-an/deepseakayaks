73cacf7074be77467369ba679136e5ca5f023fad
Eric An <erican@me.com>
2015-11-05 16:36:24 -0800

    implement redis for generating popular products list

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,6 +6,28 @@ class Product < ActiveRecord::Base
 		comments.average(:rating).to_f
 	end
 
+	# number of Product sold
+	def scored(sold)
+    if sold > self.high_score
+      $redis.zadd("highscores", sold, self.id)
+    end
+  end
+  
+	# rank (popularity) of product
+  def rank
+    $redis.zrevrank("highscores", self.id) + 1
+  end
+  
+	# highest number of Product sold
+  def high_score
+    $redis.zscore("highscores", self.id).to_i
+  end
+  
+  # top three Products sold
+  def self.top_3
+    $redis.zrevrange("highscores", 0, 2).map{|id| Product.find(id)}
+  end
+
 	validates :name, presence: true
 	  validates :description, presence: true
 	  validates :image_url, presence: true
